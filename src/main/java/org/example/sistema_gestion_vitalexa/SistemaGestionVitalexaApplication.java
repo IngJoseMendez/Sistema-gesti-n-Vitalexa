1f526b0e70259a90223119754a2f99cbc60b322e
Jose Mendez <josealbertomendez13@gmail.com>
2026-02-04 19:14:21 -0500

    arreglando zona horaria

--- src/main/java/org/example/sistema_gestion_vitalexa/SistemaGestionVitalexaApplication.java
+++ src/main/java/org/example/sistema_gestion_vitalexa/SistemaGestionVitalexaApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SistemaGestionVitalexaApplication {
 
+    @jakarta.annotation.PostConstruct
+    public void init() {
+        java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("America/Bogota"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SistemaGestionVitalexaApplication.class, args);
     }
