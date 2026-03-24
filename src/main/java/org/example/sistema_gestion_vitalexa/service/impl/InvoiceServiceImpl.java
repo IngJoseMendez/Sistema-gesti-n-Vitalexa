808b16dec79a872b98a6cca765439eec2f2b70a8
Jose Mendez <josealbertomendez13@gmail.com>
2026-03-24 14:23:16 -0500

    flete mas grande

--- src/main/java/org/example/sistema_gestion_vitalexa/service/impl/InvoiceServiceImpl.java
+++ src/main/java/org/example/sistema_gestion_vitalexa/service/impl/InvoiceServiceImpl.java
@@ -561,8 +561,8 @@ public class InvoiceServiceImpl implements InvoiceService {
                                                         + fi.getCantidad();
 
                                         com.itextpdf.layout.element.Cell itemLabelCell = new com.itextpdf.layout.element.Cell()
-                                                        .add(new Paragraph(itemDesc).setFontSize(10).setItalic()
-                                                                        .setFontColor(ColorConstants.DARK_GRAY))
+                                                        .add(new Paragraph(itemDesc).setFontSize(13).setItalic()
+                                                                        .setFontColor(ColorConstants.RED))
                                                         .setTextAlignment(TextAlignment.RIGHT)
                                                         .setBorder(null)
                                                         .setPadding(0)
